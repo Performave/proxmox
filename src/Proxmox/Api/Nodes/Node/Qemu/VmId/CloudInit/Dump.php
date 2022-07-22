b10a05ed910a55462d9b475581172b30b31fc891
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2022-07-21 19:31:28 -0500

    Fix Cloudinit dump function not accepting params

--- src/Proxmox/Api/Nodes/Node/Qemu/VmId/CloudInit/Dump.php
+++ src/Proxmox/Api/Nodes/Node/Qemu/VmId/CloudInit/Dump.php
@@ -29,8 +29,8 @@ class Dump extends PVEPathClassBase
      * @link https://pve.proxmox.com/pve-docs/api-viewer/#/nodes/{node}/qemu/{vmid}/cloudinit/dump
      * @return array|null
      */
-    public function get(): ?array
+    public function get(array $params = []): ?array
     {
-        return $this->getPve()->getApi()->get($this->getPathAdditional());
+        return $this->getPve()->getApi()->get($this->getPathAdditional(), $params);
     }
 }
